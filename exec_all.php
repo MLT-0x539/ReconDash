06aa374bf8dcc0da32e26e5173332a60e717dcdf
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-12-14 17:03:47 +0000

    Create exec_all.php

---
+++ exec_all.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(0);
+include("detect_all_userinput.php");
+include("js_endpoint_fuzzer.php");
+include("js_secrets_fuzzer.php");
+include("param_crawl_fuzz.php");
+
+$runner = $_POST['selected-options'];
+$runall = $_POST['all-selected'];
+
+if (isset($runall) && $runall == "Y" || $runall) {
+  runall(); // function to be implemented
+}
+
+if (isset($runner) {
+
+    // to do 
+}
+
+?>
