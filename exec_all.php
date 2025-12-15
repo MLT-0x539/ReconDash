03952577d330ea016a234ff20f9ae4bd09444aa9
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-12-15 16:39:23 +0000

    Add runShellScript call in exec_all.php

--- exec_all.php
+++ exec_all.php
@@ -5,12 +5,14 @@ include("detect_all_userinput.php");
 include("js_endpoint_fuzzer.php");
 include("js_secrets_fuzzer.php");
 include("param_crawl_fuzz.php");
+include("run_internal_recon_tools.php")
 
 $runner = $_POST['selected-options'];
 $runall = $_POST['all-selected'];
 
 if (isset($runall) && $runall == "Y" || $runall) {
   runall(); // function to be implemented
+  runShellScript();
 }
 
 if (isset($runner) {
