580ea44ccefdc8b243791ada35bd91e5da4115fd
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-12-17 16:58:13 +0000

    Update sh_installer.php

--- sh_installer.php
+++ sh_installer.php
@@ -20,3 +20,33 @@ else if (isset($Installer) && $installer == "N") {
 else if (!isset($Installer)) {
   echo "<br /><p><b>ERROR: </b>No value set for installation script!</p><br />";  
 } 
+
+switch ($distro) {
+    case debian:
+        echo "<br /><p>Debian selected as distro. Passing this value as argument to installation script</p>";
+        break;
+    case ubuntu:
+        echo "<br /><p>Ubuntu selected as distro. Passing this value as argument to installation script</p>";
+        break;
+    case arch:
+        echo "<br /><p>Arch selected as distro. Passing this value as argument to installation script</p>";
+        break;
+    case kali:
+        echo "<br /><p>Kali selected as distro. Passing this value as argument to installation script</p>";
+        break;
+    case RHEL:
+        echo "<br /><p>Redhat selected as distro. Passing this value as argument to installation script</p>";
+        break;
+    case Fedora:
+        echo "<br /><p>Fedora selected as distro. Passing this value as argument to installation script</p>";
+        break;
+    case Slackware:
+        echo "<br /><p>Slackware selected as distro. Passing this value as argument to installation script</p>";
+        break;
+    case Gentoo:
+        echo "<br /><p>Gentoo selected as distro. Passing this value as argument to installation script</p>";
+        break;
+    case Mint:
+        echo "<br /><p>Linux Mint selected as distro. Passing this value as argument to installation script</p>";
+        break;
+}
