89929c066bf3356c3f329344e161f3f351a0d2ed
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-12-17 16:29:11 +0000

    Add error reporting to run_internal_recon_tools.php
    Enable error reporting and display for debugging.

--- run_internal_recon_tools.php
+++ run_internal_recon_tools.php
@@ -1,6 +1,10 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 public function runShellScript() {
   // root required
   system("chmod +x recondash.sh");
   system("./recondash.sh");
+  
 }
