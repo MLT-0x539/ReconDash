dc55ccec73266bd08516ec496741c38b05fc8637
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-12-18 10:29:16 +0000

    Update recondash.sh

--- sh-scripts/recondash.sh
+++ sh-scripts/recondash.sh
@@ -1,1 +1,11 @@
-//
+#!/bin/bash
+
+if [$1 = "quick"]; then
+ // run less extensive scanning
+elif [$1 = "normal"]; then
+ // run normal scanning
+elif [$1 = "full"]; then
+ // run most extensive scanning options
+else
+ echo "ERROR: Unknown error!";
+fi
