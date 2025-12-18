00541d7c7e69e524d44b5d10840d9efd0569d752
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-12-18 10:23:17 +0000

    Update installer.sh

--- sh-scripts/installer.sh
+++ sh-scripts/installer.sh
@@ -1,1 +1,25 @@
-installation script goes here
+#!/bin/bash
+
+if [ $1 = "debian" ]; then
+ // install
+ elif [ $1 = "ubuntu" ]; then
+ // install
+ elif [ $1 = "arch" ]; then
+ // install
+ elif [ $1 = "kali" ]; then
+ // install
+ elif [ $1 = "redhat" ]; then
+ // install
+ elif [ $1 = "fedora" ]; then
+ // install
+ elif [ $1 = "slackware" ]; then
+ // install
+ elif [ $1 = "gentoo" ]; then
+ // install
+ elif [ $1 = "mint" ]; then
+ // install
+ elif [ $1 = "other" ]; then
+ // install
+else
+ echo "Error: No valid value selected!";
+fi
