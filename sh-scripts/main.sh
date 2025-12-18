50b615021aa50af22ff855928fde17208eb5d1f1
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-12-18 10:26:54 +0000

    Update main.sh

--- sh-scripts/main.sh
+++ sh-scripts/main.sh
@@ -1,1 +1,14 @@
+#/bin/bash
 
+echo "double-checking that installation has worked successfully";
+chmod +x installer.sh
+./insaller.sh
+
+if [$1 == "Y"]; then
+  chmod +x recondash.sh
+  ./recondash.sh
+elif [$1 == "N"]; then
+ echo "You chose not to run the bash scripts";
+else 
+ echo "ERROR: Unknown error has taken place";
+fi
