2feca87d5a7d90b830b6d07179b4e5787b01e88e
Matthew "MLT" Telfer <194559716+MLT-0x539@users.noreply.github.com>
2025-12-19 17:07:00 +0000

    Update sh_installer.php

--- sh_installer.php
+++ sh_installer.php
@@ -5,31 +5,53 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="/assets/installer.css">
   <title>Bash Script Installer</title>
- </head>
- <body>
+ <style>
+ 	a {
+		color: red;
+		}
+		a:hover {
+		color: green;
+		}
+		
+		.textinfo {
+		font-size: 12px;
+		}
+    </style>
+</head>
+<body>
+    <div class="container">
+	<hr>
+        <header>
+            <h1>ReconDash</h1>
+            <p class="subtitle"><b>Bug0xF4</b> - <i>Reconnaissance Command Center</i></p>
+        </header>
+		<hr>
+		<br />
   <center>
+  		<div class="dashboard-links">
+		<button onclick="window.location='recondash.html'"><b>RETURN TO MAIN PAGE</b></button></a><br />
+		</div>
+		<br /><br />
    <h2>Required Bash Script Installer:</h2>
+   </center>
+   <div class="textinfo">
    <p>This script will download and install the required bash scripts onto your VPS. Many web-based aspects of this recon tool can still be used without the bash scripts, but for the tool
    to reach its full potential, you should also install these bash scripts for more extensive recon capabilities. In order to install these, select 'Y' as the option on the drop-down menu
    asking you if you want to install these, and select the Linux distribution that is running on your VPS. If you cannot see your Linux Distribution listed as an option within the drop-down
    menu, then just choose an option that uses the same package manager as your distro. If you aren't sure which option that would be, then take a look at the following list:</p>
-   <br />
    <a href="https://en.wikipedia.org/wiki/List_of_Linux_distributions">List of distros and their corresponding package managers</a>
-  </center>
+   </div>
    <br /><br />
-    <form action="sh_installer.php" method="POST">
-      Do you want to install the Bash Scripts onto your machine?:<input name="installer" type="installer"><br />
+<form action="sh_installer.php" method="POST">
+<b>Would you like to run the installer for the local tools on your machine?</b><br />
+  <input type="checkbox" id="yes" name="yes" value="yes">
+  <label for="yes"> Yes</label><br>
+  <input type="checkbox" id="no" name="no" value="no">
+  <label for="no"> No</label><br>
+   <br />
+      <b>Select your distro from the following menu:</b><br />
       <select>
-        <option value="Y">Yes</option>
-        <option value="N">No</option>
-      <input type="submit" value="submit">
-     </select>
-     </form>
-   <br /><br />
-    <form action="sh_installer.php" method="POST">
-      Select your distro from the drop-down menu:<input name="distro" type="distro"><br />
-      <select>
-        <option value="debian">Debkan</option>
+        <option value="debian">Debian</option>
         <option value="ubuntu">Ubuntu</option>
         <option value="arch">Arch</option>
         <option value="kali">Kali</option>
@@ -38,9 +60,11 @@
         <option value="Gentoo">Gentoo</option>
         <option value="Slackware">Slackware</option>
         <option value="Mint">Mint</option>
+		<br />
       <input type="submit" value="submit">
      </select>
-     </form>
+	 </form>
+	 </div>
  </body> 
 </html>
 
